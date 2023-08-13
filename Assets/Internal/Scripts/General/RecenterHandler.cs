f9e194c608f192fda1c1d2bc2f70c7b5eb188cb8
shoshanimayan <mshoshan@uci.edu>
2023-08-12 19:54:28 -0700

    recenter fix

--- Assets/Internal/Scripts/General/RecenterHandler.cs
+++ Assets/Internal/Scripts/General/RecenterHandler.cs
@@ -5,67 +5,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Management;
 using UnityEngine.XR;
 using Unity.XR.CoreUtils;
+using UnityEngine.InputSystem;
 
 namespace General
 {
 	public class RecenterHandler : MonoBehaviour
 	{
-		//  PRIVATE VARIABLES         //
-
-		private XRLoader _xrLoader;
-
-		//  PRIVATE METHODS           //
-
-
-		private void Start()
+		[SerializeField] private Transform _target;
+		private void Recenter()
 		{
-			var xrSettings = XRGeneralSettings.Instance;
-			if (xrSettings == null)
-			{
-				Debug.LogError($"XRGeneralSettings is null.");
-				return;
-			}
-			var xrManager = xrSettings.Manager;
-			if (xrManager == null)
-			{
-				Debug.LogError($"XRManagerSettings is null.");
-				return;
-			}
-
-			_xrLoader = xrManager.activeLoader;
-			if (_xrLoader == null)
-			{
-				Debug.LogError($"XRLoader is null.");
-				return;
-			}
-			ResetRig();
+			XROrigin xROrigin = GetComponent<XROrigin>();
+			xROrigin.MoveCameraToWorldLocation(_target.position);
+			xROrigin.MatchOriginUpCameraForward(_target.up, _target.forward);
 		}
 
-		private IEnumerator ResetPosition()
-		{
-			yield return new WaitForEndOfFrame();
-			FindObjectOfType<XROrigin>().transform.eulerAngles = new Vector3(0, 0, 0);
-			FindObjectOfType<XROrigin>().transform.position = new Vector3(0, 0, 0);
-			var xrInput = _xrLoader.GetLoadedSubsystem<XRInputSubsystem>();
-
-			if (xrInput != null)
-			{
-				xrInput.TrySetTrackingOriginMode(TrackingOriginModeFlags.Device);
-				xrInput.TryRecenter();
-				xrInput.TrySetTrackingOriginMode(TrackingOriginModeFlags.Unknown);
-
-			}
-			else
-			{
-				Debug.LogError($"XRInput: {xrInput != null}");
-			}
-		}
-
-		//  PUBLIC API               //
-
-		public void ResetRig()
-		{
-			StartCoroutine(ResetPosition());
-		}
 	}
 }
