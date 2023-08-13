8a653751e6cfc9f34497a6e385cbae2cb8bd42b5
shoshanimayan <mshoshan@uci.edu>
2023-08-12 21:31:23 -0700

    fix recentering issues

--- Assets/Internal/Scripts/General/RecenterHandler.cs
+++ Assets/Internal/Scripts/General/RecenterHandler.cs
@@ -11,12 +11,65 @@ namespace General
 {
 	public class RecenterHandler : MonoBehaviour
 	{
+		private void Start()
+		{
+			Recenter();
+
+		}
+
+		private void Awake()
+		{
+			Recenter();
+		}
+
+		private void OnEnable()
+		{
+			Recenter();
+		}
+
+		private void OnBecameVisible()
+		{
+			Recenter();
+
+		}
+
+		
+
+		private bool _recentered;
+
+
 		[SerializeField] private Transform _target;
+		[SerializeField] private Transform _head;
+		[SerializeField] private Transform _origin;
+
+		[SerializeField] private InputActionProperty _recenterButton;
+
 		private void Recenter()
 		{
-			XROrigin xROrigin = GetComponent<XROrigin>();
-			xROrigin.MoveCameraToWorldLocation(_target.position);
-			xROrigin.MatchOriginUpCameraForward(_target.up, _target.forward);
+			Vector3 offset = _head.position - _origin.position;
+			offset.y = 0;
+			_origin.position = _target.position - offset;
+			Vector3 targetForward = _target.forward;
+			targetForward.y = 0;
+			Vector3 cameraForwrd = _head.forward;
+			cameraForwrd.y = 0;
+
+			float angle = Vector3.SignedAngle(cameraForwrd, targetForward, Vector3.up);
+			_origin.RotateAround(_head.position, Vector3.up, angle);
+		}
+
+		private void Update()
+		{
+			if (!_recentered)
+			{
+				_recentered = true;
+				Recenter();
+			}
+
+			if (_recenterButton.action.WasPerformedThisFrame())
+			{
+				Recenter();
+			}
 		}
 
 	}
